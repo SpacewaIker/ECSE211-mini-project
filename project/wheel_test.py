506545002f4d91c8846efe717a4afd925ef008c4
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-11-08 14:48:04 -0500

    wheel test

--- project/wheel_test.py
+++ project/wheel_test.py
@@ -18,7 +18,7 @@ def main():
             inp = input("press to move a bit")
             if (inp == "x"):
                 break
-            WHEEL_MOTOR.set_position_relative(30)
+            WHEEL_MOTOR.set_position_relative(100)
     except KeyboardInterrupt:
         print("Ending program")
     except Exception as e:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
